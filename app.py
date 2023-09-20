b0788a4ed0cdbbaa2afc9151af93b0f901aee26a
blackduckdevil <blackduckdevil@gmail.com>
2023-09-20 12:57:44 +0000

    dynamic pages for each job

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, json
-from database import load_jobs_from_db
+from database import load_jobs_from_db, load_job_from_db
 
 app = Flask(__name__)
 
@@ -15,6 +15,15 @@ def list_jobs():
   jobs = load_jobs_from_db()
   return json.dumps(jobs, default=dict)
 
+@app.route("/job/<id>")
+def show_job(id):
+  job = load_job_from_db(id)
+  
+  if not job:
+    return "NOT FOUND", 404
+  
+  return render_template('jobpage.html', 
+                          job=job[0])
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
--- database.py
+++ database.py
@@ -19,3 +19,17 @@ def load_jobs_from_db():
     for row in result.all():
       jobs.append(row._mapping)
     return jobs
+
+def load_job_from_db(id):
+  with engine.connect() as conn:
+    result = conn.execute(
+      text("SELECT * FROM jobs WHERE id = :val"), 
+      {"val":id}
+    )
+    rows = result.all()
+    job = []
+    if len(rows) == 0:
+      return None
+    else:
+      job.append(rows[0]._mapping)
+      return job
