21a77c2315ee185653697ac71a811d86514a08b4
blackduckdevil <blackduckdevil@gmail.com>
2023-10-03 14:53:06 +0000

    store application in database

--- database.py
+++ database.py
@@ -33,3 +33,16 @@ def load_job_from_db(id):
     else:
       job.append(rows[0]._mapping)
       return job
+
+def add_application_to_db(job_id, data):
+  with engine.connect() as conn:
+    query = text("INSERT INTO application (job_id, full_name, email, linkedin_url, education, work_experience, resume_url) VALUES (:job_id, :full_name, :email, :linkedin_url, :education, :work_experience, resume_url)")
+
+    conn.execute(query,
+                  {"job_id":job_id, 
+                   "full_name":data['full_name'], 
+                   "email":data['email'], 
+                   "linkedin_url":data['linkedin_url'], 
+                   "education":data['education'], 
+                   "work_experience":data['work_experience'], 
+                   "resume_url":data['resume_url']})
